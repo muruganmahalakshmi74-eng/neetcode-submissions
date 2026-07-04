b0e8b5af1addfa47386f6550742fa5892f6c62bc
muruganmahalakshmi74-eng <muruganmahalakshmi74@gmail.com>
2026-07-04 21:14:01 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+      let map = {}
+      for(let num of nums){
+        if(!map[num]) map[num]=0
+        map[num]++
+      }
+      let res = [...Object.keys(map)].sort((a,b)=>map[b]-map[a]).slice(0,k)
+      return res
+    }
+}
